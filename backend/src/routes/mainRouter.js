06eae1c2647c835f069c0d61e58e5fdb37ca1d13
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-04-01 15:38:51 +0300

    added file uploading and downloading

--- backend/src/routes/mainRouter.js
+++ backend/src/routes/mainRouter.js
@@ -9,6 +9,7 @@ const tagsRouter = require("./routers/tagsRouter")
 const categoriesRouter = require("./routers/categoriesRouter")
 const vacanciesRouter = require("./routers/vacancyRouter")
 const applicationsRouter = require("./routers/applicationRouter")
+const resumesRouter = require("./routers/resumeRouter")
 
 router.use('/users', userRouter)
 router.use('/workers', workerRouter)
@@ -18,5 +19,7 @@ router.use('/tags', tagsRouter)
 router.use('/categories', categoriesRouter)
 router.use('/vacancies', vacanciesRouter)
 router.use('/applications', applicationsRouter)
+router.use('/resumes', resumesRouter)
+
 
 module.exports = router
---
+++ backend/src/routes/routers/resumeRouter.js
@@ -0,0 +1,62 @@
+const Resume = require("../../models/resume")
+const express = require("express")
+const path = require("path")
+const multer = require('multer')
+
+const uploadDirectory = 'uploads/'
+
+const upload = multer({
+    dest: uploadDirectory,
+    fileFilter: (req, file, callback) => {
+        let ext = path.extname(file.originalname);
+        if (ext !== '.pdf') {
+            return callback(new Error('Only pdf allowed'))
+        }
+        callback(null, true)
+    }
+}).single('resume')
+
+
+const router = express.Router()
+
+router.get("/", async (req, res) => {
+    try {
+        const users = await Resume.find({})
+        res.status(200).send(users)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.post("/", upload, async (req, res) => {
+    if (!req.body) {
+        res.status(400).send()
+    }
+
+    let resumeData = req.body
+    const newResume = new Resume(resumeData)
+
+    try {
+        // await newResume.save()
+        res.status(200).send(newResume)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.get("/download/:id", async (req, res) => {
+
+    let fileName = req.params.id
+
+    let filePath = uploadDirectory + fileName
+    const newFileName = fileName + '.pdf'
+
+    try {
+        res.status(200).download(filePath, newFileName)
+    } catch ({message}) {
+
+    }
+})
+
+
+module.exports = router
