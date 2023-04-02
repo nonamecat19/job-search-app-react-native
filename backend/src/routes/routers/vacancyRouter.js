1bfc060a610b6dffdb66c2143448d28e19a75774
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-04-02 16:28:37 +0300

    added search for vacancy

--- backend/src/routes/routers/vacancyRouter.js
+++ backend/src/routes/routers/vacancyRouter.js
@@ -12,6 +12,36 @@ router.get("/", async (req, res) => {
     }
 })
 
+router.get("/search", async (req, res) => {
+    try {
+        const filter = req.query
+        const newFilter = {}
+
+        if (filter.minSalary) {
+            newFilter.minSalary = {
+                $lte: filter.minSalary
+            }
+        }
+        if (filter.experience) {
+            newFilter.experience = {
+                $lte: filter.experience
+            }
+        }
+        if (filter.query) {
+            newFilter.title = {
+                $regex: filter.query,
+                $options: 'i'
+            }
+        }
+
+        const vacancies = await Vacancy.find(newFilter)
+        res.status(200).send(vacancies)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
 router.post("/", async (req, res) => {
     if (!req.body) {
         res.status(400).send()
