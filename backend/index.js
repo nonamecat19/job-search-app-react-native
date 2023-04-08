311877f20ef531b37e4416945662cea966b0f3be
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-04-08 13:57:03 +0300

    2st try deploy backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-const mainRouter = require("./src/routes/mainRouter")
 require("./src/db/mongoose.js")
 
 const app = express()
