8f2ac29d70a19cd289a833d88fff87235846f4c8
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-04-12 00:35:49 +0300

    saves improvement

--- frontend/components/common/search/index.tsx
+++ frontend/components/common/search/index.tsx
@@ -37,25 +37,30 @@ const SearchCard: FC<Props> = ({data}) => {
 
     return (
         <TouchableOpacity style={styles.element}
-            onPress={() => router.push(`/vacancies/${id}`)}
+                          onPress={() => router.push(`/vacancies/${id}`)}
         >
-            <Text>{title}</Text>
-            <Text>{location ?? ''}</Text>
-            <View>
-                <Text>{company.name}</Text>
-                <Image
-                    source={{
-                        uri: checkImageURL(company?.logo)
-                            ? company?.logo
-                            : `https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg`
-                    }}
-                    style={{
-                        width: 40,
-                        height: 40
-                    }}
-                />
-                <Text>{getSalary()}</Text>
+            <View style={styles.row}>
+                <Text style={styles.title}>{title}</Text>
+                <Text>{location ?? 'Віддалено'}</Text>
             </View>
+            <View style={styles.row}>
+                <View style={styles.companyContainer}>
+                    <Text style={styles.company}>{company.name}</Text>
+                    <Image
+                        source={{
+                            uri: checkImageURL(company?.logo)
+                                ? company?.logo
+                                : `https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg`
+                        }}
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </View>
+                <Text style={styles.salary}>{getSalary()}</Text>
+            </View>
+
 
         </TouchableOpacity>
     )
--- frontend/components/common/search/searchCard.style.js
+++ frontend/components/common/search/searchCard.style.js
@@ -4,8 +4,30 @@ import {COLORS, SIZES} from "../../../constants"
 const styles = StyleSheet.create({
     element: {
         backgroundColor: COLORS.white,
-        padding: 20,
-        borderRadius: 20
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        paddingBottom: 15,
+        borderRadius: 20,
+    },
+    companyContainer: {
+        flexDirection: "row"
+    },
+    company: {
+        color: COLORS.primary,
+        fontSize: 18,
+        textDecorationLine: "underline"
+    },
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20,
+        color: COLORS.primary
+    },
+    salary: {
+        color: COLORS.primary
     }
 })
 export default styles
